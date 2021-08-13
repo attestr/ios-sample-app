f341cffab95de5e11335c1f345d56767794a20c9
Tushar Kalra <tushar.gupta0303@gmail.com>
2021-08-13 12:33:16 +0530

    First commit

---
+++ FlowxApp/Extensions.swift
@@ -0,0 +1,77 @@
+//
+//  Extensions.swift
+//  FlowxApp
+//
+//  Created by Tushar Kalra
+//  Copyright (c) 2021 Pegadroid IQ Solutions Pvt. Ltd.
+
+import Foundation
+import UIKit
+
+extension UITextField {
+    func setLeftPaddingPoints(_ amount:CGFloat){
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: amount, height: self.frame.size.height))
+        self.leftView = paddingView
+        self.leftViewMode = .always
+    }
+    func setRightPaddingPoints(_ amount:CGFloat) {
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: amount, height: self.frame.size.height))
+        self.rightView = paddingView
+        self.rightViewMode = .always
+    }
+}
+
+extension UIView {
+    
+    public var width: CGFloat{
+        return self.frame.size.width
+    }
+    public var height: CGFloat{
+        return self.frame.size.height
+    }
+    public var top: CGFloat{
+        return self.frame.origin.y
+    }
+    public var bottom: CGFloat{
+        return self.frame.size.height + self.frame.origin.y
+    }
+    public var left: CGFloat{
+        return self.frame.origin.x
+    }
+    
+    public var right: CGFloat{
+        return self.frame.size.width + self.frame.origin.x
+    }
+}
+
+
+extension UIColor {
+    
+    convenience init(hexString: String, alpha: CGFloat = 1.0) {
+        let hexString: String = hexString.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+        let scanner = Scanner(string: hexString)
+        if (hexString.hasPrefix("#")) {
+            scanner.scanLocation = 1
+        }
+        var color: UInt32 = 0
+        scanner.scanHexInt32(&color)
+        let mask = 0x000000FF
+        let r = Int(color >> 16) & mask
+        let g = Int(color >> 8) & mask
+        let b = Int(color) & mask
+        let red   = CGFloat(r) / 255.0
+        let green = CGFloat(g) / 255.0
+        let blue  = CGFloat(b) / 255.0
+        self.init(red:red, green:green, blue:blue, alpha:alpha)
+    }
+    
+    func toHexString() -> String {
+        var r:CGFloat = 0
+        var g:CGFloat = 0
+        var b:CGFloat = 0
+        var a:CGFloat = 0
+        getRed(&r, green: &g, blue: &b, alpha: &a)
+        let rgb:Int = (Int)(r*255)<<16 | (Int)(g*255)<<8 | (Int)(b*255)<<0
+        return String(format:"#%06x", rgb)
+    }
+}
